d1ae97f1ca23e2d72e7e8a45c7286c974199b8b2
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-10-06 23:34:47 +0200

    fix(deps): dev-preview.21

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,17 @@
 import LatexPreview, { LatexPreviewProps } from './components/LatexPreview';
-import { createPlugin, defineType, type PreviewValue } from 'sanity';
+import { createPlugin } from 'sanity';
+import { latexSchema, LatexDefinition } from './schema';
 
-export { LatexPreview, type LatexPreviewProps };
+export {
+  LatexPreview,
+  latexSchema,
+  type LatexPreviewProps,
+  type LatexDefinition,
+};
 
 export const latexInput = createPlugin({
   name: 'latex-block-plugin',
   schema: {
-    types: [
-      defineType({
-        type: 'object',
-        name: 'latex',
-        components: {
-          preview: LatexPreview,
-        },
-        fields: [
-          {
-            title: 'LaTeX content',
-            name: 'body',
-            type: 'text',
-          },
-        ],
-        preview: {
-          select: {
-            body: 'body',
-          },
-          prepare(selection: PreviewValue) {
-            return selection;
-          },
-        },
-      }),
-    ],
+    types: [latexSchema],
   },
 });
---
+++ src/schema.ts
@@ -0,0 +1,42 @@
+import { defineType, ObjectDefinition, type PreviewValue } from 'sanity';
+import LatexPreview from './components/LatexPreview';
+
+const latexTypeName = 'latex' as const;
+
+/**
+ * @public
+ */
+export interface LatexDefinition
+  extends Omit<ObjectDefinition, 'type' | 'fields'> {
+  type: typeof latexTypeName;
+}
+
+declare module '@sanity/types' {
+  // makes type: 'latex' narrow correctly when using defineType/defineField/defineArrayMember
+  export interface IntrinsicDefinitions {
+    latex: LatexDefinition;
+  }
+}
+
+export const latexSchema = defineType({
+  type: 'object',
+  name: 'latex',
+  components: {
+    preview: LatexPreview,
+  },
+  fields: [
+    {
+      title: 'LaTeX content',
+      name: 'body',
+      type: 'text',
+    },
+  ],
+  preview: {
+    select: {
+      body: 'body',
+    },
+    prepare(selection) {
+      return selection as unknown as PreviewValue;
+    },
+  },
+});
